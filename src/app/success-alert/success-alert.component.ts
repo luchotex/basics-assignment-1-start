695c8207c0b542ba4156b84665b8f48afa47c10c
Luis Kupferberg Ruiz <luis.kupferberg@bairesdev.com>
2022-10-12 11:12:43 -0400

    Initial commit

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,16 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'success-alert',
+  templateUrl: './success-alert.component.html',
+  styles: [`
+    h3 {
+      padding: 20px;
+      color: green;
+      border: 1px solid green;
+    }
+  `]
+})
+export class SuccessAlertComponent {
+
+}
---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,18 @@
+import {Component} from '@angular/core';
+
+
+@Component({
+  selector: 'warning-alert',
+  templateUrl: './warning-alert.component.html',
+  styles: [
+    `
+    h3 {
+      padding: 20px;
+      color: red;
+      border: 1px solid red;
+    }`
+  ]
+})
+export class WarningAlertComponent {
+
+}
